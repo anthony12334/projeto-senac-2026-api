97b63af44f7ab39fe8138f00d15e46f330fb2c8f
ANTHONY SANT ANNA DA SILVA <60251578046@senacrs.edu.br>
2026-07-01 17:25:12 -0300

    create users

--- viajei_api/setting.py
+++ viajei_api/setting.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='uft-8'
+        env_file='.env', env_file_encoding='utf-8'
     )
 
-    # DATABASE_URL: str = Field(init=False, default_factory=)
+    DATABASE_URL: str = Field(init=False)
